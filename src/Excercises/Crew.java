e97d6104d0458d0ea09ca3b1ab3b5869e4ea3fa0
alex <ajvallee15@gmail.com>
2021-04-26 21:26:10 +0100

    Refactor

--- src/Excercises/Crew.java
+++ src/Excercises/Crew.java
@@ -11,28 +11,39 @@ public class Crew extends Astronaut{
     private static final String[] RANKS = { "Commander", "Pilot", "Payload Commander", "Mission specialist", "Spaceflight Participant"};
     private String[] nationalities;
     private List<Astronaut> crew = new ArrayList<>();
-    public static String filepath = ("/home/alex/Java/src/Assignment/nationalities.txt");
+    public static String filepath = ("/home/alex/Java/src/Excercises/nationalities.txt");
     ///home/alex/Java/src/Assignment/
 
     public Crew(String nationality, String rank, int rankNum, int age, String filepath) {
         super(nationality, rank, rankNum, age);
         this.filepath = filepath;
+        this.rankNum = RANKS[crew.get(0).getRankNum()];
 
         String currentRank = RANKS[crew.get(0).getRankNum()];
 
+        this.nationalities = getNationalityArray(this.filepath);
 
+        for (int i = 0; i <= this.nationalities.length; i++){
+            for (int j = 0; j < this.RANKS.length; j++) {
+
+            }
+        }
     }
 
+    /*
     public static void main(String[] args) {
 
         for (int i = 0; i < Crew.getNationalityArray(filepath).length; i++) {
-            System.out.println(Crew.getNationalityArray(filepath).toString());
+            String[] tmp = Crew.getNationalityArray(filepath);
+            System.out.println(tmp[i].toString());
         }
 
     }
 
+     */
 
-    public static String[] getNationalityArray(String filepath) {
+
+    public String[] getNationalityArray(String filepath) {
         ArrayList<String> arr = new ArrayList<>();
         try {
             Scanner s = new Scanner(new File(filepath));
@@ -49,8 +60,17 @@ public class Crew extends Astronaut{
         return items;
     }
 
-    @Override
-    public String toString() {
-        return String.format()
+    public int randomAge() {
+        int age = 0;
+        for (int i = 0; i <= 1; i++) {
+            age = (int) (Math.random() * (55 - 32 + 1));
+        }
+        return age;
     }
+
+    public void PrintCrew(){
+
+    }
+
+
 }
