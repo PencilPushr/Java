97561cf776b0f6e8ec4b815e064d33b2553aa333
alex <ajvallee15@gmail.com>
2021-06-30 14:53:43 +0100

    Project 20 done

--- src/Euler/P20/p20.java
+++ src/Euler/P20/p20.java
@@ -3,12 +3,15 @@ package Euler.P20;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class p20 {
 
     public static void main(String[] args) {
 
-        BigInteger bigint, runningTotal, spare;
+        p20 project = new p20();
+
+/*        BigInteger bigint, runningTotal, spare;
         bigint = new BigInteger("1");
         runningTotal = new BigInteger("0");
         spare = new BigInteger("0");
@@ -22,21 +25,72 @@ public class p20 {
             runningTotal = runningTotal.add(bigint.mod(BigInteger.valueOf(10)));
             bigint = bigint.divide(BigInteger.valueOf(10));
         }
-        System.out.println(runningTotal);
+        System.out.println(runningTotal);*/
 
-        //see if it's possible to solve using arrays
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        int[] a = new int[1];
-        Arrays.fill(a, 1);
-        int localV = 1;
+        int FactorialInput = 5;
+        project.LongMultiplicationFactorial(FactorialInput);
+
+    }
+
+    public int[] LongMultiplicationFactorial(int x){
 
 
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 0; j < a.length; j++) {
-                a[j] = a[j] * i;
+        int tempInput = x;
+
+        //if we reach the fourth loop and the if statement is not initialised, we've fucked up
+        for (int i = tempInput; i > 0; i--) {
+            int[] array = new int[i];
+            for (int j = 0; j < i; j++) {
+
+                array[i] = array[i] * tempInput;
+                if (array[i] > 10){
+                    array[i] = array[i] % 10;
+                    array[i+1] = array[i] /10;
+                }
+
+                //array[i] = array[i] * array[i+1];
+
+                //in the if statement we may have to reassign array[i] to temp[i]
+                //as we may not want to fully reassign the array until the last moment before return from this funciton.
+
+
             }
         }
-        System.out.println(localV);
+
+        int[] c = new int[];
+
+        for
+
+
+        return;
+    }
+
+    public int[] arrayMultiply(int x, int[] y){
+
+        int temp = x;
+        int[] tempArray = y;
+        int[] b;
+
+        Collections.reverse(Arrays.asList(y));
+
+        for (int i:tempArray) {
+            tempArray[i] = tempArray[i] * temp;
+        }
+
+        for (int i = 0; i < tempArray.length-1; i++) {
+
+            if (tempArray[i] > 10){
+                tempArray[i+1] = tempArray[i+1] + tempArray[i];
+                //tempArray[i+1] = tempArray[i] / 10;
+                tempArray[i] = tempArray[i] % 10;
+            }
+
+            if (tempArray[tempArray.length-1] > 10){
+                b = new int[tempArray.length+1];
+            }
+        }
+
+
     }
 
 }
