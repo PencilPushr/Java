f13df141f8f3f3bb1d9b3585b4a7d90be84bf6c9
alex <ajvallee15@gmail.com>
2021-08-03 22:52:47 +0100

    new updates

--- src/Excercises2/MRIData.java
+++ src/Excercises2/MRIData.java
@@ -1,4 +1,50 @@
 package Excercises2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class MRIData {
+
+    private int[][] arrayOfMRI2D;
+    private int[][][] arrayOfMRI3D;
+
+    public static void main(String[] args) {
+        try{
+            readData2D();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void readData2D() throws FileNotFoundException {
+
+        TwoDimentionalArrayList tdal = new TwoDimentionalArrayList();
+        List<List<String>> al = new ArrayList<List<String>>();
+        String temp = null;
+
+
+        Scanner s = new Scanner(new File("/home/alex/Java/src/Excercises2/mri/mri/151.csv"));
+        s.useDelimiter(",");
+
+        while(s.hasNextLine()){
+            temp = s.next();
+            al.get(i).add(temp);
+        }
+
+        System.out.println(Arrays.toString(al.toArray()));
+
+
+    }
+
+    public int getIntensity2D(int coordinates){
+        int temp = 0;
+
+        return temp;
+    }
+
 }
---
+++ src/Excercises2/MainMRI.java
@@ -0,0 +1,13 @@
+package Excercises2;
+
+import javax.swing.*;
+
+public class MainMRI {
+
+    public static void main(String[] args) {
+
+
+        JFrame j = new JFrame("Drawing");
+    }
+
+}
