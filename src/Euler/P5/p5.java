c9e56629ea24c34548155dca610f8b545bcf1499
archveragejoe <ajvallee15@gmail.com>
2021-09-04 14:25:27 +0100

    project 5

---
+++ src/Euler/P5/p5.java
@@ -0,0 +1,36 @@
+package Euler.P5;
+
+public class p5 {
+
+    public p5() {
+        System.out.println(this.counter());
+    }
+
+    public static void main(String[] args) {
+        new p5();
+    }
+
+    public boolean divisor(int x){
+        int y = x;
+
+        //should look at GCD algorithm as referenced from a StackExchange post.
+        for (int i = 11; i < 20; i++) { //i can also start at 2
+            int temp = y;
+            if ((temp = temp % i) != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int counter(){
+        int currentNumber = 2520;
+
+        while(!divisor(currentNumber)){
+            currentNumber++;
+        }
+
+        return currentNumber;
+    }
+}
