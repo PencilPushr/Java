ee1b9a7048b1f3eedb865347d7da38b60c8c27ab
archveragejoe <ajvallee15@gmail.com>
2021-09-07 12:27:37 +0100

    p7

---
+++ src/Euler/P7/p7.java
@@ -0,0 +1,34 @@
+package Euler.P7;
+
+public class p7 {
+
+    public p7(){
+        primeLoop();
+    }
+
+    public static void main(String[] args) {
+        new p7();
+    }
+
+    public void primeLoop(){
+        int primeCounter = 2;
+        int number = 3;
+
+        while(primeCounter < 10001){
+            number += 2;
+            if (isPrime(number)){
+                primeCounter++;
+            }
+        }
+        System.out.println(number);
+    }
+
+    public boolean isPrime(int x){
+        for (int i = 3; i <= Math.sqrt(x); i+=2) {
+            if (x % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
