a0c76ce44ae32840002db20042f969e623c64034
archveragejoe <ajvallee15@gmail.com>
2022-02-23 01:53:17 +0000

    finished write randomletters.

--- src/Labs/Lab9/labexercise2/Letter2DIO.java
+++ src/Labs/Lab9/labexercise2/Letter2DIO.java
@@ -1,8 +1,9 @@
 package Labs.Lab9.labexercise2;
 
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Letter2DIO {
 
@@ -11,21 +12,46 @@ public class Letter2DIO {
 
 
     public static void writeRandomLetters(String filename, int noObjects) throws FileNotFoundException {
-        PrintStream output = new PrintStream(filename);
+        //PrintStream output = new PrintStream(filename);
+        PrintWriter pw = new PrintWriter(filename + ".csv");
+        StringBuilder sb = new StringBuilder();
+        //List<String[]> items = new List<String[]>;
+        //String[] temp
+        //alternatively using temp I can append them to items and then once I have all the arrays of objects
+        //put them in items and then push them into the file.
+
         for (int i = 0; i < noObjects; i++) {
-            //output.println(math.random); -- placeholder
+            sb.append(generateRndIntX());
+            sb.append(",");
+            sb.append(generateRndIntY());
+            sb.append(",");
+            sb.append(generateRndChar());
+            sb.append("\n");
+            pw.write(sb.toString());
         }
-        output.close();
+        //output.close();
+        pw.close();
     }
 
     public static Letter2D[] readLetters(String filename, int noOfLines){
+        Scanner s = new Scanner(filename);
+        for (int i = 0; i < noOfLines; i++) {
 
+        }
         return new Letter2D[0];
     }
 
-    private char generateRndChar(){
+    private static char generateRndChar(){
         Random r = new Random();
         //char c = (char)(r.nextInt(26) + 'a');
         return (char)(r.nextInt(26) + 'a');
+    }
+
+    private static int generateRndIntX(){
+        return (0 + (int) (Math.random() * ((maxX - 0) + 1)));
+    }
+
+    private static int generateRndIntY(){
+        return (0 + (int)(Math.random() * ((maxY - 0) + 1)));
     }
 }
