650e1a5762b64168f17db0cc734279998ebcec4b
AverageWindows <ajvallee15@gmail.com>
2022-04-23 18:32:49 +0200

    dumb shit added correct classes to new exerises

---
+++ src/Exercises3/ex4/CityGUI.java
@@ -0,0 +1,8 @@
+package Exercises3.ex4;
+
+import java.awt.event.MouseAdapter;
+
+public class CityGUI implements MouseAdapter {
+
+
+}
---
+++ src/Exercises3/ex4/MainCityGUI.java
@@ -0,0 +1,8 @@
+package Exercises3.ex4;
+
+import javax.swing.*;
+
+public class MainCityGUI {
+
+    JFrame j = new JFrame();
+}
